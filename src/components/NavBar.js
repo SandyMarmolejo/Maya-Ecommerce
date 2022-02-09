1e592e3cddb31ef5d821057bdfa77743b6e0b4bb
sandymarmolejo <sandymarmolejoc@gmail.com>
2022-02-08 19:45:04 -0500

    menu con secciones de logo y elementos de menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,40 @@
-import { AppBar, Wrapper, Logo, MenuItem, Left, Center, Right } from '@material-ui/core'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const useStyles = makeStyles((theme) => ({
+   root: {
+     flexGrow: 1,
+   },
+   menuButton: {
+     marginRight: theme.spacing(2),
+   },
+   title: {
+     flexGrow: 1,
+   },
+   menu: {
+      flexGrow: 2,
+    },
+ }));
 
 const Navbar = () => {
+   const classes = useStyles();
    return (
-   <AppBar position="fixed">
-      <Wrapper>
-        <Left>
-          <Logo>MAYA</Logo>
-        </Left>
-        <Center>
-           <MenuItem>Categorias</MenuItem>
-           <MenuItem>Ofertas</MenuItem>
-           <MenuItem>Novedades</MenuItem>
-           <MenuItem>Contacto</MenuItem>
-        </Center>
-       <Right>
-
-       </Right>
-      </Wrapper>
-   </AppBar>
+      <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Logo Maya
+          </Typography>
+          <MenuItem className={classes.menu}>Inicio</MenuItem>
+          <MenuItem className={classes.menu}>Categoria</MenuItem>
+          <MenuItem className={classes.menu}>Ofertas</MenuItem>
+        </Toolbar>
+      </AppBar>
+    </div>
    );
 }
 
