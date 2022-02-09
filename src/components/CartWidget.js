0beb816321818bf45739239dac81b832ce14c4dd
sandymarmolejo <sandymarmolejoc@gmail.com>
2022-02-08 22:03:18 -0500

    contador de agregar productos al carrito

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,11 +1,23 @@
 import { Badge } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import { useState } from 'react';
 
 const CartWidget = () => {
+    // let addProducts = 0;
+
+    const [ addProduct, setAddProduct ] = useState(0);
+
+    const increment = () => {
+        setAddProduct(addProduct+1);
+    }
+
     return (
-        <Badge badgeContent={8} color="secondary">
-           < AddShoppingCartIcon />
+        <Button onClick={increment}>
+         <Badge badgeContent={addProduct} color="secondary">
+            < AddShoppingCartIcon />
         </Badge>
+        </Button>
     )
 }
 
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,23 +1,11 @@
 
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-      '& > * + *': {
-        marginLeft: theme.spacing(2),
-      },
-    },
-  }));
 
 const ItemListContainer = () => {
-    const classes = useStyles();
-  const preventDefault = (event) => event.preventDefault();
     return (
-        <Typography className={classes.root}>
-    <span>Texto</span>
-    </Typography>
+        <div>
+            <span>Holaaaa</span>
+        </div>
     )
 }
 
